939b8817b0c7646731b5fe0e72bf67ff88326e40
ninkaninus <williamstrand8@gmail.com>
2015-02-27 10:04:27 +0100

    BestTimefunction added to read and write from EEPROM, fixes #5

---
+++ SubProjects/ScalextrixLapTimer/ScalextrixLapTimer.ino
@@ -0,0 +1,155 @@
+
+
+/********************************************/
+/************Team Turbo Turtle***************/
+/************Proudly Presents****************/
+/*************The Time Taker!****************/
+/********************************************/
+
+
+/*****************Defines********************/
+
+//Define the data output modes
+#define STARTUP_MODE 0 //This mode is for when the program is uninitialized, primarily for debugging.
+#define RAW_MODE 1 //This mode only sends the laptime over serial. For use with other programs. Outputs the lap time in ms.
+#define SERIAL_MODE 2 //This mode is for using an external serial monitor as a display. Displays: Lap #, Lap Time and Total Time. In a human readable and understandable format.
+
+//Define char inputs for serial communication
+#define BYTE_COMPARE_R 114 //ASCII value for r
+#define BYTE_COMPARE_S 115 //ASCII value for s
+#define BYTE_COMPARE_E 101 //ASCII value for e
+#define BYTE_COMPARE_H 104 //ASCII value for h
+#define BYTE_COMPARE_D 100 //ASCII value for d
+
+//Defines for I/O
+#define SENSOR_PIN A0 //The pin where the button is connected. The program is designed for a button with a pulldown configuration.
+
+//Defines for time constants
+#define DEBOUNCE_TIME 250 //The time in ms used for debouncing the switch.
+#define BAUD_RATE 115200
+
+//Defines for sensor compensation
+#define HALL_HYSTERESE 15
+
+/*****************Variables*******************/
+
+//For laptime calculations
+unsigned long firstTime;// a place to store the first real time the sensor war trigged
+boolean firstLap; //a bool to hol if the first run has been preformed
+unsigned long lastTime;// place to store the last time the sensor war trigged
+unsigned long newTime;// place to store the newest time the sensor war trigged
+unsigned long lapTime;//place to store the calculated lap time
+unsigned long firstlapTime; //place to store time of the first real lap
+unsigned long lapCount; //place to store the number of laps 
+
+//Misc
+byte byteRead;// place to store the read byte
+int mode = STARTUP_MODE;
+int hallCompensate = 0;
+
+/*****************Functions********************/
+
+void setup(){
+  Serial.begin(BAUD_RATE); // start the serial at 115200 baud
+  pinMode(SENSOR_PIN,INPUT); //set the pin to an input
+  Startup();
+  mode = RAW_MODE;
+  hallCompensate = analogRead(SENSOR_PIN);
+}
+
+void Startup() {
+  firstLap = true; //set that the first lap has not been run
+  firstlapTime = 0; //set time for the first lap time (used to calculate total time on all laps)
+  lapCount = 0;//zero the lap counter
+  lastTime = millis(); // get the time the arduino started
+  Serial.write(12);//Clear the terminal
+}
+
+void loop(){
+  if(Serial.available()){ //do this if there is something at the serial port
+    ReadSerial();
+  }
+
+  if ((analogRead(SENSOR_PIN)-hallCompensate) >= HALL_HYSTERESE){ //compare the value to find out if the light is blocked
+    CountLap();  
+  }
+}
+
+void CountLap() {
+  newTime = millis(); // store the time the light was blocked
+  if (firstLap == true){  //do this if it is the first lap
+    firstTime = newTime; //store the first time the light gets blocked
+  }
+
+  if (mode == SERIAL_MODE){
+    lapTime = newTime - lastTime; //calcuate the lap time
+    if (firstLap == true){
+      lapTime = firstTime - lapTime;
+      firstLap = false; //set the bool to true because now the first lap had been run
+    }
+    firstlapTime = newTime - firstTime; //calculate the time since the first lap was runned
+    if (lapCount > 0){
+      String stringOut = "";
+      stringOut = String("Lap ") + lapCount + String("\tLap time: ") + lapTime + String("\tTotal Time: ") + firstlapTime;
+      Serial.println(stringOut);
+    }
+    lastTime = newTime; //set the newest time to the last time to be ready to store the new time when it comes
+  }
+
+  if (mode == RAW_MODE){
+    lapTime = newTime - lastTime; //calcuate the lap time
+    if (firstLap == true){
+      firstLap = false; //set the bool to true because now the first lap had been run
+    }
+    if (lapCount > 0){
+      Serial.println(lapTime); // print to serial the lap time 
+    }
+    lastTime = newTime; //set the newest time to the last time to be ready to store the new time when it comes
+  }
+  lapCount ++; //add one to the lap count
+  delay(DEBOUNCE_TIME); //delay so we do not track the object more then once  
+}
+
+void ReadSerial() {
+  byteRead = Serial.read(); // store the byte send into the serial
+
+  switch(byteRead) {
+  case BYTE_COMPARE_R:
+    Startup();
+    break; 
+
+  case BYTE_COMPARE_S:
+    mode = SERIAL_MODE;
+    break;
+
+  case BYTE_COMPARE_E:
+    mode = RAW_MODE;
+    break;
+
+  case BYTE_COMPARE_H:
+    Serial.println("Press s to read in serial mode");
+    Serial.println("Press e to read in external mode");
+    Serial.println("Press r to reset data");
+    Serial.println("press v to see debug values");
+    Serial.println("press h to see help");
+    break;
+
+  case BYTE_COMPARE_D:
+    Serial.println("Now in debug mode!");
+    Serial.println(String("Hall Compensate Value: ") + hallCompensate);
+    Serial.println(String("Hall Hysterese Value: ") + HALL_HYSTERESE);
+    Serial.println("Now outputting hall sensor value: ");
+    while(!Serial.available()) {
+      Serial.println(analogRead(SENSOR_PIN));
+      delay(250);
+    }
+    ReadSerial();
+    Startup();
+    break;
+  }
+}
+
+
+
+
+
