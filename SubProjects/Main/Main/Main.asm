8959a4c1609763dd9631f4aa6841a82ac7f0b718
StjerneIdioten <dragonest7@gmail.com>
2015-04-06 21:47:43 +0200

    Minor update

--- SubProjects/Main/Main/Main.asm
+++ SubProjects/Main/Main/Main.asm
@@ -6,8 +6,8 @@
 ; 
 
 ;Defines
-.equ I2C_W = 0b11010000
-.equ I2C_R = 0b11010001
+.equ I2C_W = 0x3C
+.equ I2C_R = 0x3D
 .def DATA_HIGH = R19
 .def DATA_LOW = R20
 
@@ -28,6 +28,14 @@ Init:
 	LDI	R16, high(RAMEND)
     OUT	SPH, R16			
 	
+	;Enable internal weak-pullups on sda and scl
+	/*
+	ldi R16, (0<<PC0)|(0<<PC1)
+	out DDRC, R16
+	ldi R16, (1<<PC0)|(1<<PC1)
+	out PORTC, R16
+	*/
+
 	USART_Init
 	Motor_Init
 	I2C_Init 0x00,0x12	;Prescaler 4 and TWBR 12
@@ -36,40 +44,6 @@ Init:
 	rjmp	Main
 
 Main:
-	call I2C_Start
-
-	ldi R16, I2C_W
-	call I2C_Write
-
-	ldi R16, 0x3B
-	call I2C_Write
-
-	call I2C_Start
-
-	ldi R16, I2C_R
-	call I2C_Write
-
-	ldi R16, I2C_ACK
-	call I2C_Read
-
-	mov DATA_HIGH, R16
-
-	ldi R16,I2C_NACK
-	call I2C_Read
-
-	mov DATA_LOW, R16
-
-	call I2C_Stop
-
-	mov R16, DATA_HIGH
-	call USART_Binary
-
-	mov R16, DATA_LOW
-	call USART_Binary
-
-	USART_Newline
-	
-	DELAY_MS 250
 	
 rjmp	MAIN
 
