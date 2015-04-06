fa6b19c7fe501a761e47bc9a64da916f38012340
StjerneIdioten <dragonest7@gmail.com>
2015-04-07 00:58:24 +0200

    First interaction with the mpu-6050. Got it to return it's own address.

--- SubProjects/Main/Main/Main.asm
+++ SubProjects/Main/Main/Main.asm
@@ -6,8 +6,9 @@
 ; 
 
 ;Defines
-.equ I2C_W = 0x3C
-.equ I2C_R = 0x3D
+
+.equ I2C_W = 0b11010000
+.equ I2C_R = 0b11010001
 .def DATA_HIGH = R19
 .def DATA_LOW = R20
 
@@ -15,10 +16,12 @@
 rjmp Init
 
 ;Board specific port names
+.include "MPU-6050.inc"
 .include "Delay_Macros.asm"
 .include "Motor_Control.asm"
 .include "USART_Library.asm"
 .include "I2C.asm"
+
 
 Init:
 	
@@ -39,11 +42,39 @@ Init:
 	USART_Init
 	Motor_Init
 	I2C_Init 0x00,0x12	;Prescaler 4 and TWBR 12
-	DELAY_MS 1			;Give the modules some time to start up
+
+	DELAY_MS 10			;Give the modules some time to start up
 
 	rjmp	Main
 
 Main:
 	
+	call I2C_Start
+
+	ldi R16, I2C_W
+	call I2C_Write
+
+	ldi R16, MPU6050_RA_WHO_AM_I
+	call I2C_Write
+
+	call I2C_Start
+
+	ldi R16, I2C_R
+	call I2C_Write
+
+	ldi R16,I2C_NACK
+	call I2C_Read
+
+	mov DATA_HIGH, R16
+
+	call I2C_Stop
+
+	mov R16, DATA_HIGH
+	call USART_Binary
+
+	USART_Newline
+	
+	DELAY_MS 250
+
 rjmp	MAIN
 
