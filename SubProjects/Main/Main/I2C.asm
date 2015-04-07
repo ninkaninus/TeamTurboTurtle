a02e550e2de557d9aaca048bf2f659844ce5b56d
StjerneIdioten <dragonest7@gmail.com>
2015-04-07 16:11:29 +0200

    Working I2C communication with the MPU-6050! As of now the code just outputs the raw 16 bit binary value of the gyroes z-axis, to the terminal.

--- SubProjects/Main/Main/I2C.asm
+++ SubProjects/Main/Main/I2C.asm
@@ -18,6 +18,8 @@
 	ldi R16, (1<<TWEN)
 	out TWCR, R16
 
+	nop
+
  .ENDMACRO
 
 
@@ -44,36 +46,46 @@ ret
 
 
 ;This function writes the data from R16, onto the I2C data line
-I2C_Write:
+I2C_Write_Call:
 	out TWDR, r16
 	ldi r16, (1<<TWINT) | (1<<TWEN)
 	out TWCR, r16
 
-I2C_Write_Wait:
+I2C_Write_Call_Wait:
 	in r16,TWCR
 	sbrs r16,TWINT
-	rjmp I2C_Write_Wait
+	rjmp I2C_Write_Call_Wait
 ret
 
+;This is an easy to use macro of the write function
+.MACRO I2C_Write
+	ldi R16, @0
+	call I2C_Write_Call
+.ENDMACRO
 
 ;This function reads in data from the I2C data line and stores it in R16.
 ;Remember to set R16 to either I2C_ACK or I2C_NACK before calling the function!
 ;Failure to do so may result in unwanted behaviour.
-I2C_Read:
+I2C_Read_Call:
 	sbrc r16,0
-	rjmp I2C_Read_ACK
+	rjmp I2C_Read_Call_ACK
 	ldi r16, (1<<TWINT) | (1<<TWEN)
-	rjmp I2C_Read_Continue
-I2C_Read_ACK:
+	rjmp I2C_Read_Call_Continue
+I2C_Read_Call_ACK:
 	ldi r16, (1<<TWINT) | (1<<TWEN) | (1<<TWEA)
-I2C_Read_Continue:
+I2C_Read_Call_Continue:
 	out TWCR, r16
-I2C_Read_Wait:
+I2C_Read_Call_Wait:
 	in r16,TWCR
 	sbrs r16,TWINT
-	rjmp I2C_Read_Wait
+	rjmp I2C_Read_Call_Wait
 
 	in R16, TWDR
 ret
+
+.MACRO I2C_Read
+	ldi R16, @0
+	call I2C_Read_Call
+.ENDMACRO
 
 
--- SubProjects/Main/Main/Main.asm
+++ SubProjects/Main/Main/Main.asm
@@ -16,12 +16,12 @@
 rjmp Init
 
 ;Board specific port names
-.include "MPU-6050.inc"
 .include "Delay_Macros.asm"
 .include "Motor_Control.asm"
 .include "USART_Library.asm"
 .include "I2C.asm"
-
+.include "MPU-6050.inc"
+.include "MPU-6050.asm"
 
 Init:
 	
@@ -30,20 +30,14 @@ Init:
     OUT	SPL, R16
 	LDI	R16, high(RAMEND)
     OUT	SPH, R16			
-	
-	;Enable internal weak-pullups on sda and scl
-	/*
-	ldi R16, (0<<PC0)|(0<<PC1)
-	out DDRC, R16
-	ldi R16, (1<<PC0)|(1<<PC1)
-	out PORTC, R16
-	*/
 
 	USART_Init
 	Motor_Init
 	I2C_Init 0x00,0x12	;Prescaler 4 and TWBR 12
-
-	DELAY_MS 10			;Give the modules some time to start up
+	MPU6050_Init
+	ldi R16, 'D'
+	call USART_Transmit
+	USART_Newline
 
 	rjmp	Main
 
@@ -51,31 +45,35 @@ Main:
 	
 	call I2C_Start
 
-	ldi R16, I2C_W
-	call I2C_Write
+	I2C_Write I2C_W
 
-	ldi R16, MPU6050_RA_WHO_AM_I
-	call I2C_Write
+	I2C_Write MPU6050_RA_GYRO_ZOUT_H
 
 	call I2C_Start
 
-	ldi R16, I2C_R
-	call I2C_Write
+	I2C_Write I2C_R
 
-	ldi R16,I2C_NACK
-	call I2C_Read
+	I2C_Read I2C_Ack
 
 	mov DATA_HIGH, R16
 
-	call I2C_Stop
+	I2C_Read I2C_Nack
 
-	mov R16, DATA_HIGH
+	mov DATA_LOW, R16
+
+	call I2C_Stop
 	
+	mov R16, DATA_HIGH
+
+	call USART_Binary
+
+	mov R16, DATA_LOW
+
 	call USART_Binary
 
 	USART_Newline
-	
-	DELAY_MS 250
 
+	DELAY_MS 250
+	
 rjmp	MAIN
 
