3f6ffd7c97251919a5f2b7c1ef9df70de64278e7
StjerneIdioten <dragonest7@gmail.com>
2015-04-13 14:26:12 +0200

    Lot's of new stuff: Data pull from the sensor to flash

--- SubProjects/Main/Main/SRAM-Mapping.asm
+++ SubProjects/Main/Main/SRAM-Mapping.asm
@@ -1,6 +1,22 @@
-/*
- * SRAM_Mapping.asm
- *
- *  Created: 13-04-2015 12:43:37
- *   Author: StjerneIdioten
- */ 
+;Mapping of the data memory
+;SRAM starts at 0x60
+
+;Accelerometer dataset from the MPU6050
+.equ ACCEL_XOUT_H = 0x0060
+.equ ACCEL_XOUT_L = 0x0061
+.equ ACCEL_YOUT_H = 0x0062
+.equ ACCEL_YOUT_L = 0x0063
+.equ ACCEL_ZOUT_H = 0x0064
+.equ ACCEL_ZOUT_L = 0x0065
+
+;Temperature dataset from the MPU6050
+.equ TEMP_OUT_H   = 0x0066
+.equ TEMP_OUT_L   = 0x0067
+
+;Gyro dataset from the MPU6050
+.equ GYRO_XOUT_H  = 0x0068
+.equ GYRO_XOUT_L  = 0x0069
+.equ GYRO_YOUT_H  = 0x006A
+.equ GYRO_YOUT_L  = 0x006B
+.equ GYRO_ZOUT_H  = 0x006C
+.equ GYRO_ZOUT_L  = 0x006D
