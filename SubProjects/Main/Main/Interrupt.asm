bc42b07795b7b0de578079174b16fd75ea6c3080
Gugfann <kmolha@gmail.com>
2015-04-14 14:18:38 +0200

    Removed fluff

--- SubProjects/Main/Main/Interrupt.asm
+++ SubProjects/Main/Main/Interrupt.asm
@@ -4,7 +4,4 @@
 			
 			ldi		R16, (1<<OCIE0)			; enable interrupt on output compare match for timer0
 			out		TIMSK, R16				; timer/interrupt masking register
-			
-			ldi		R16, (1<<INT0)			; enable external interrupt INT0
-			out		GICR, R16				; global interrupt register
 
