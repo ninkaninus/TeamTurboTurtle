4ab1eccc22bca557db1147c15226cbf29a692348
Gugfann <kmolha@gmail.com>
2015-05-06 14:02:26 +0200

    Wheelspeed fix mk1. Identificering af lorten og efterfølgende afskaffelse af denne

--- SubProjects/Main/Main/WheelSpeed.asm
+++ SubProjects/Main/Main/WheelSpeed.asm
@@ -16,6 +16,9 @@
 			ldi R16, 'P'
 			call USART_Transmit
 			USART_Newline
+			
+			ldi		YL, low(Pulse_Time_L1)
+			ldi		YH, high(Pulse_Time_L1)
 
 .ENDMACRO
 
@@ -28,6 +31,7 @@
 			sts		Wheel_speed_H, R17
 .ENDMACRO
 
+; Use desired braking time in MS as argument
 .MACRO Brake_MS
 		in		R16, OCR0
 		push	R16
@@ -58,8 +62,8 @@ Input_Capture:
 			;ldi R16, 'D'
 			;call USART_Transmit
 
-			lds		R16, SREG_1
-			sbrc	R16, 0
+			lds		R16, SREG_1			
+			sbrc	R16, 0							; bit 0 represents the current edge that's being measured - 0 = EDGE1, 1 = EDGE2
 			rjmp	EDGE2
 			
 EDGE1:		in		R0, ICR1L
@@ -90,9 +94,9 @@ EDGE2:		lds		R0, Edge1_L
 			
 			sub		R2, R0
 			sbc		R3, R1
-			
-			sts		Pulse_Time_L, R2
-			sts		Pulse_Time_H, R3
+						
+			st		Y+, R2
+			st		Y+, R3
 			
 			ldi		R16, 0x00
 			out		TCNT1H, R16						; Temp = R16
@@ -102,8 +106,57 @@ EDGE2:		lds		R0, Edge1_L
 			cbr		R16, 0b00000001					; clear bit 0 in R16 (performs logical AND with complement of operand)
 			sts		SREG_1, R16
 			
+; Compares the last two pulse times and checks to see if they're below a set threshold
 
+			cpi		YL, low(Pulse_Time_H2)			; jump to end if pointer register is not set to Pulse_Time_H2
+			brne	end
+			
+			lds 	R0, Pulse_Time_L1
+			lds 	R1, Pulse_Time_H1
+			
+			lds		R2, Pulse_Time_L2
+			lds		R3, Pulse_Time_H2
+			
+			cp		R1, R3							; compares high bytes to see if Pulse1_H < Pulse2_H
+			brlo	P1_LO
+			
+			cp		R1, R3							; checks to see if high bytes are equal
+			brne	P2_LO
 
+			cp		R2, R0							; if high bytes are equal, low bytes are compared to see if Pulse2_L < Pulse1_L
+			brlo	P2_LO
+			
+; Pulse 1 < Pulse 2			
+P1_LO:		sub		R2, R0
+			sbc		R3, R1
+			
+			cpi		R3, high(6000)
+			brlo	NO_GO
+			
+			rjmp	GO
+			
+; Pulse 2 < Pulse 1			
+P2_LO:		sub		R0, R2
+			sbc		R1, R3
+			
+			cpi		R1, high(6000)
+			brlo	NO_GO
+			
+Go:			lds		R2, Pulse_Time_L2								; Latest pulse time is considered valid and stored for use
+			lds		R3, Pulse_Time_H2
+			
+			sts		Pulse_Time_L, R2
+			sts		Pulse_Time_H, R3 
+			
+			ldi		YL, low(Pulse_Time_L1)							; Pointer register is reset
+			ldi		YH, high(Pulse_Time_L1)
+
+			rjmp	end
+			
+NO_GO:		ldi		YL, low(Pulse_Time_L1)							; Pointer register is reset
+			ldi		YH, high(Pulse_Time_L1)
+
+end:
 			lds R16, Pulse_Time_L
 			lds R17, Pulse_Time_H
 
@@ -128,7 +181,7 @@ EDGE2:		lds		R0, Edge1_L
 			end1:
 
 			pop		R16
-			out		SREG
+			out		SREG, R16
 			pop		R16
 			pop		R3
 			pop		R2
